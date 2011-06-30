f4affd71beef3c0aca70b9f8a679440f6820490d
Andreas Akesson <andreas.akesson@ericsson.com>
2011-06-30 17:38:09 +0300

    First working version

--- src/main/java/com/ericsson/extjob/MonitoredJob.java
+++ src/main/java/com/ericsson/extjob/MonitoredJob.java
@@ -1,16 +1,17 @@
 package com.ericsson.extjob;
 
-
 import hudson.model.RunMap.Constructor;
 import hudson.Extension;
 import hudson.model.AbstractProject;
 import hudson.model.Descriptor.FormException;
 import hudson.model.Hudson;
+import hudson.model.Item;
 import hudson.model.ItemGroup;
 import hudson.model.TopLevelItem;
 import hudson.model.TopLevelItemDescriptor;
 import hudson.model.ViewJob;
 import hudson.util.AlternativeUiTextProvider;
+import hudson.widgets.Widget;
 import java.io.File;
 import java.io.IOException;
 //import java.lang.reflect.Constructor;
@@ -18,12 +19,12 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
-
-
 
 /*
  * To change this template, choose Tools | Templates
@@ -53,7 +54,6 @@ import org.kohsuke.stapler.StaplerResponse;
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 /**
  * Job that runs outside Hudson whose result is submitted to Hudson
  * (either via web interface, or simply by placing files on the file system,
@@ -61,26 +61,28 @@ import org.kohsuke.stapler.StaplerResponse;
  *
  * @author Kohsuke Kawaguchi
  */
-public class MonitoredJob extends ViewJob<MonitoredJob,MonitoredRun> implements TopLevelItem {
-
+public class MonitoredJob extends ViewJob<MonitoredJob, MonitoredRun> implements TopLevelItem {
 
     private static final int THREAD_STATUS_OK = 0;
     private static final int THREAD_STATUS_ERROR = 1;
-
-    private String jobURL = null;
-    private int threadStatus = THREAD_STATUS_OK;
+    private String subscriptionUrl = null;
+    private transient int threadStatus = THREAD_STATUS_OK;
+    private transient SubscriptionStatusWidget ssw = null;
 
     /**
      * Threading class which refreshes the keepalive of the subscription
      */
-    private class SubscriptionKeepAlive extends Thread {
+    private class SubscriptionKeepAlive implements Runnable {
 
-        public static final int KEEPALIVE_TIME = 1000*60*5; // 5 minutes
-        MonitoredJob parent = null;
-        boolean isRunning = true;
+        public static final int KEEPALIVE_TIME = 2000; //1000*60*5; // 5 minutes
+        private transient MonitoredJob parentJob = null;
+        private transient volatile boolean isRunning = true;
 
-        SubscriptionKeepAlive(MonitoredJob parent) {
-            this.parent = parent;
+        SubscriptionKeepAlive(MonitoredJob parentJob) {
+            
+            this.parentJob = parentJob;
+            if (this.parentJob == null)
+                isRunning = false;
         }
 
         void terminate() {
@@ -90,81 +92,123 @@ public class MonitoredJob extends ViewJob<MonitoredJob,MonitoredRun> implements 
         @Override
         public void run() {
 
-            // TODO: Add sleep and loop
+                while (isRunning) {
+                    String subscriptionURL = parentJob.getSubscriptionUrl();
 
-            if (parent != null) {
-                String subscriptionURL = parent.getSubscriptionUrl();
+                    if (subscriptionURL != null) {
+                        URL url = null;
 
-                if (subscriptionURL != null) {
-                    URL url = null;
-
-                    try {
-                        url = new URL(parent.getSubscriptionUrl() + "subscription/subscribe"); // subscriber already has a tailing slash
-                    } catch (MalformedURLException e) {
-                        System.out.println("Erronous subscription URL (" + parent.getSubscriptionUrl() + "subscription/subscribe )");
-                        parent.reportThreadStatus(THREAD_STATUS_ERROR);
-                        return;
-                    }
-
-                    try {
-                        HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
-                        httpCon.setDoOutput(true);
-                        httpCon.setRequestMethod("POST");
-                        OutputStreamWriter out = new OutputStreamWriter(
-                                httpCon.getOutputStream());
-                        out.write(parent.getAbsoluteUrl() );
-                        out.flush();
-
-                        // We got an error of some kind..
-                        if (httpCon.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                            // Remove subscriber
-
+                        try {
+                            url = new URL(parentJob.getSubscriptionUrl() + "subscription/subscribe"); // subscriber already has a tailing slash
+                        } catch (MalformedURLException e) {
+                            System.out.println("Erronous subscription URL (" + parentJob.getSubscriptionUrl() + "subscription/subscribe )");
+                            parentJob.reportThreadStatus(THREAD_STATUS_ERROR);
+                            return;
                         }
 
-                        out.close();
-                    } catch (Exception e) {
-                        System.out.println("Unable to connect to server");
-                        parent.reportThreadStatus(THREAD_STATUS_ERROR);
-                        return;
+                        try {
+                            
+                            HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+                            httpCon.setDoOutput(true);
+                            httpCon.setRequestMethod("POST");
+                            OutputStreamWriter out = new OutputStreamWriter(
+                                    httpCon.getOutputStream());
+                            out.write(Hudson.getInstance().getRootUrl() + parentJob.getUrl());
+                            out.flush();
+
+                            // We got an error of some kind..
+                            if (httpCon.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                                parentJob.reportThreadStatus(THREAD_STATUS_ERROR);
+                                System.out.println("Error???");
+                            } else {
+                                parentJob.reportThreadStatus(THREAD_STATUS_OK);
+                            }
+
+                            out.close();
+
+                        } catch (Exception e) {
+                            System.out.println("Unable to connect to server: " + e.getMessage());
+                            parentJob.reportThreadStatus(THREAD_STATUS_ERROR);
+                        }
+
+
                     }
-
-                    System.out.println("Subscription OK");
-                    parent.reportThreadStatus(THREAD_STATUS_OK);
+                    try {
+                        Thread.sleep(KEEPALIVE_TIME);
+                    } catch (InterruptedException ex) {}
                 }
-
-            }
-                
+            
         }
     }
-
-    private SubscriptionKeepAlive keepAliveThread = null;
+    private transient SubscriptionKeepAlive keepAliveThread = null;
 
     public MonitoredJob(String name) {
-        this(Hudson.getInstance(),name);
+        this(Hudson.getInstance(), name);
     }
 
     public MonitoredJob(ItemGroup parent, String name) {
-        super(parent,name);
+        super(parent, name);
     }
+/*
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+
+        // Not sure if this function is ever called
+        if (keepAliveThread != null) {
+            keepAliveThread.terminate();
+        }
+    }
+*/
 
     public String getSubscriptionUrl() {
-        return jobURL;
+        return subscriptionUrl;
     }
 
     private void reportThreadStatus(int status) {
+        threadStatus = status;
+
+        if (ssw == null)
+            ssw = new SubscriptionStatusWidget();
+
+        if (status == THREAD_STATUS_OK)
+            ssw.setStatusOk(true);
+        else
+            ssw.setStatusOk(false);
 
     }
 
     @Override
-    protected void reload() {
-        this.runs.load(this,new Constructor<MonitoredRun>() {
-            public MonitoredRun create(File dir) throws IOException {
-                return new MonitoredRun(MonitoredJob.this,dir);
-            }
-        });
+    public void onLoad(ItemGroup<? extends Item> parent, String name)
+                                                    throws IOException {
+        super.onLoad(parent, name);
+        if (subscriptionUrl != null) {
+            keepAliveThread = new SubscriptionKeepAlive(this);
+            Thread t = new Thread(keepAliveThread);
+            t.setName(this.getDisplayName() + " update thread");
+            t.start();
+        }
+    }
+
+    @Override
+    public List<Widget> getWidgets() {
+        if (ssw == null)
+            ssw = new SubscriptionStatusWidget();
+        ArrayList<Widget> r = new ArrayList<Widget>();
+        r.add(ssw);
+        return r;
     }
 
 
+    @Override
+    protected void reload() {
+        this.runs.load(this, new Constructor<MonitoredRun>() {
+
+            public MonitoredRun create(File dir) throws IOException {
+                return new MonitoredRun(MonitoredJob.this, dir);
+            }
+        });
+    }
     // keep track of the previous time we started a build
     private transient long lastBuildStartTime;
 
@@ -201,7 +245,7 @@ public class MonitoredJob extends ViewJob<MonitoredJob,MonitoredRun> implements 
     /**
      * Used to post the build result from a remote machine.
      */
-    public void doPostBuildResult( StaplerRequest req, StaplerResponse rsp ) throws IOException, ServletException {
+    public void doPostBuildResult(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
         checkPermission(AbstractProject.BUILD);
         MonitoredRun run = newBuild();
         run.acceptRemoteSubmission(req.getReader());
@@ -220,45 +264,48 @@ public class MonitoredJob extends ViewJob<MonitoredJob,MonitoredRun> implements 
             //Logger.getLogger(XFPanelView.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        jobURL = req.getParameter("subscriptionURL");
+        subscriptionUrl = req.getParameter("subscriptionURL");
 
-        if (jobURL != null) {
+        if (subscriptionUrl != null) {
             // Add a tailing slash, if it is missing
-            if (!jobURL.endsWith("/"))
-                jobURL += "/";
+            if (!subscriptionUrl.endsWith("/")) {
+                subscriptionUrl += "/";
+            }
         }
 
-        System.out.println("Got job URL " + jobURL);
+        //System.out.println("Got job URL " + subscriptionUrl);
         // Start the update thread - kill previous one if needed
-        if (keepAliveThread != null)  {
+        if (keepAliveThread != null) {
             keepAliveThread.terminate();
             keepAliveThread = null;
         }
 
-        System.out.println("Starting thread..");
-        //keepAliveThread = new SubscriptionKeepAlive(this);
-        //keepAliveThread.start();
-        System.out.println("Thread started");
+        //System.out.println("Monitored job " + this.getDisplayName() + " trying to subscripe to " + subscriptionUrl);
+
+        keepAliveThread = new SubscriptionKeepAlive(this);
+        Thread t = new Thread(keepAliveThread);
+        t.setName(this.getDisplayName() + " update thread");
+        t.start();
+
+        save();
 
     }
-
     @Extension
     public static final TopLevelItemDescriptor DESCRIPTOR = new DescriptorImpl();
 
     @Override
     public String getPronoun() {
-        return AlternativeUiTextProvider.get(PRONOUN, this, "Job");//Messages.MonitoredJob_Pronoun());
+        return AlternativeUiTextProvider.get(PRONOUN, this, "Monitored job");//Messages.MonitoredJob_Pronoun());
     }
 
     public static final class DescriptorImpl extends TopLevelItemDescriptor {
+
         public String getDisplayName() {
             return "Monitor an external Jenkins job"; //Messages.MonitoredJob_DisplayName();
         }
 
         public MonitoredJob newInstance(ItemGroup parent, String name) {
-            return new MonitoredJob(parent,name);
+            return new MonitoredJob(parent, name);
         }
     }
-
 }
-
--- src/main/java/com/ericsson/extjob/SubscriptionCollector.java
+++ src/main/java/com/ericsson/extjob/SubscriptionCollector.java
@@ -11,8 +11,6 @@ package com.ericsson.extjob;
 /*
  * The MIT License
  *
- * Copyright (c) 2004-2010, Sun Microsystems, Inc., Kohsuke Kawaguchi,
- * Jean-Baptiste Quenot, Seiji Sogabe, Tom Huybrechts
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -128,6 +126,10 @@ public class SubscriptionCollector
 
         String addr = reader.readLine();
 
+        if (addr == null) {
+            rsp.sendError(rsp.SC_BAD_REQUEST, "No subscription address provided");
+        }
+
         // Add a tailing slash, if it is missing
         if (!addr.endsWith("/")) {
             addr += "/";
@@ -141,20 +143,25 @@ public class SubscriptionCollector
         }
 
         // If the subscriber is already on the list, just beat it (tm)
-        for (JobSubscriber subscriber : subscriberList) {
+        if (subscriberList != null) {
 
-            if (subscriber.getSubscriber().equals(addr)) {
-                System.out.println("Subscriber " + subscriber.getSubscriber() + " renewed its lease.");
-                subscriber.beatIt();
-                return createRedirectToMainPage();
+            for (JobSubscriber subscriber : subscriberList) {
+
+                if (subscriber.getSubscriber().equals(addr)) {
+                    //System.out.println("Subscriber " + subscriber.getSubscriber() + " renewed its lease.");
+                    subscriber.beatIt();
+                    return createRedirectToMainPage();
+                }
+
             }
-
         }
+        else
+            subscriberList = new ArrayList<JobSubscriber>();
 
         // Othervise create a new subscriber
         JobSubscriber subscriber = new JobSubscriber(addr);
         addSubscriber(subscriber);
-        System.out.println("New subscriber " + addr + " added.");
+        //System.out.println("New subscriber " + addr + " added.");
 
         return createRedirectToMainPage();
     }
@@ -165,243 +172,3 @@ public class SubscriptionCollector
 }
 
 
-
-
-
-/**
- * Keeps a list of the parameters defined for a project.
- *
- * <p>
- * This class also implements {@link Action} so that <tt>index.jelly</tt> provides
- * a form to enter build parameters.
- */
-//
-//@ExportedBean(defaultVisibility=2)
-//public class SubscriptionCollector extends JobProperty<AbstractProject<?, ?>>
-//        implements Action {
-//
-//    private final List<ParameterDefinition> parameterDefinitions;
-//
-//    public SubscriptionCollector(List<ParameterDefinition> parameterDefinitions) {
-//        System.out.println("Construct1");
-//        this.parameterDefinitions = parameterDefinitions;
-//    }
-//
-//    public SubscriptionCollector(ParameterDefinition... parameterDefinitions) {
-//        System.out.println("Construct2");
-//        this.parameterDefinitions = Arrays.asList(parameterDefinitions);
-//    }
-//
-//    public AbstractProject<?,?> getOwner() {
-//        System.out.println("owner");
-//        return owner;
-//    }
-//
-//    @Exported
-//    public List<ParameterDefinition> getParameterDefinitions() {
-//        System.out.println("paramter definition");
-//        return parameterDefinitions;
-//    }
-//
-//    /**
-//     * Gets the names of all the parameter definitions.
-//     */
-//    public List<String> getParameterDefinitionNames() {
-//        System.out.println("getparametersdefinitions");
-//        return new AbstractList<String>() {
-//            public String get(int index) {
-//                return parameterDefinitions.get(index).getName();
-//            }
-//
-//            public int size() {
-//                return parameterDefinitions.size();
-//            }
-//        };
-//    }
-//
-//    @Override
-//    public Collection<Action> getJobActions(AbstractProject<?, ?> job) {
-//        System.out.println("Get actions");
-//        return Collections.<Action>singleton(this);
-//    }
-//
-//    public AbstractProject<?, ?> getProject() {
-//        System.out.println("Get project");
-//        return (AbstractProject<?, ?>) owner;
-//    }
-//
-//    /**
-//     * Interprets the form submission and schedules a build for a parameterized job.
-//     *
-//     * <p>
-//     * This method is supposed to be invoked from {@link AbstractProject#doBuild(StaplerRequest, StaplerResponse)}.
-//     */
-//    public void _doBuild(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
-//
-//        System.out.println("_dobuild");
-//        if(!req.getMethod().equals("POST")) {
-//            // show the parameter entry form.
-//            req.getView(this,"index.jelly").forward(req,rsp);
-//            return;
-//        }
-//
-//        List<ParameterValue> values = new ArrayList<ParameterValue>();
-//
-//        JSONObject formData = req.getSubmittedForm();
-//        JSONArray a = JSONArray.fromObject(formData.get("parameter"));
-//
-//        for (Object o : a) {
-//            JSONObject jo = (JSONObject) o;
-//            String name = jo.getString("name");
-//
-//            ParameterDefinition d = getParameterDefinition(name);
-//            if(d==null)
-//                throw new IllegalArgumentException("No such parameter definition: " + name);
-//            ParameterValue parameterValue = d.createValue(req, jo);
-//            values.add(parameterValue);
-//        }
-//
-//    	//Jenkins.getInstance().getQueue().schedule(
-//        //        owner, owner.getDelay(req), new ParametersAction(values), new CauseAction(new Cause.UserCause()));
-//
-//        // send the user back to the job top page.
-//        rsp.sendRedirect(".");
-//    }
-//
-//    public void doFsck(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
-//
-//        System.out.println("FSCK!");
-//    }
-//
-//    public void buildWithParameters(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
-//
-//        System.out.println("buildWithParamters");
-//
-//        List<ParameterValue> values = new ArrayList<ParameterValue>();
-//        for (ParameterDefinition d: parameterDefinitions) {
-//        	ParameterValue value = d.createValue(req);
-//        	if (value != null) {
-//        		values.add(value);
-//        	} else {
-//        		throw new IllegalArgumentException("Parameter " + d.getName() + " was missing.");
-//        	}
-//        }
-//
-//    	//Hudson.getInstance().getQueue().schedule(
-//        //        owner, owner.getDelay(req), new ParametersAction(values), owner.getBuildCause(req));
-//
-//        // send the user back to the job top page.
-//        rsp.sendRedirect(".");
-//    }
-//
-//    /**
-//     * Gets the {@link ParameterDefinition} of the given name, if any.
-//     */
-//    public ParameterDefinition getParameterDefinition(String name) {
-//        System.out.println("Get parameters definition");
-//        for (ParameterDefinition pd : parameterDefinitions)
-//            if (pd.getName().equals(name))
-//                return pd;
-//        return null;
-//    }
-//
-//    @Extension
-//    public static class DescriptorImpl extends JobPropertyDescriptor {
-//        @Override
-//        public boolean isApplicable(Class<? extends Job> jobType) {
-//            System.out.println("Is applicable!!!!!!!!!!!! =" + AbstractProject.class.isAssignableFrom(jobType));
-//            return AbstractProject.class.isAssignableFrom(jobType);
-//        }
-//
-//        @Override
-//        public JobProperty<?> newInstance(StaplerRequest req,
-//                                          JSONObject formData) throws FormException {
-//
-//            System.out.println("New instance called!");
-//            if (formData.isNullObject()) {
-//                return null;
-//            }
-//
-//            JSONObject parameterized = formData.getJSONObject("parameterized");
-//
-//            if (parameterized.isNullObject()) {
-//            	return null;
-//            }
-//
-//            List<ParameterDefinition> parameterDefinitions = Descriptor.newInstancesFromHeteroList(
-//                    req, parameterized, "parameter", ParameterDefinition.all());
-//            if(parameterDefinitions.isEmpty())
-//                return null;
-//
-//            return new SubscriptionCollector(parameterDefinitions);
-//        }
-//
-//        @Override
-//        public String getDisplayName() {
-//            return "DISPLAY NAME!"; //Messages.SubscriptionCollector_DisplayName();
-//        }
-//    }
-//
-//    public String getDisplayName() {
-//        return null;
-//    }
-//
-//    public String getIconFileName() {
-//        return null;
-//    }
-//
-//    public String getUrlName() {
-//        return null;
-//    }
-//}
-
-
-
-/*
-@ExportedBean(defaultVisibility=2)
-public class SubscriptionCollector implements PermalinkProjectAction {
-
-    @DataBoundConstructor
-    SubscriptionCollector() {
-        System.out.println("Construction");
-    }
-
-    public void doSubscribe(org.kohsuke.stapler.StaplerRequest req,
-                            org.kohsuke.stapler.StaplerResponse rsp)
-                            throws IOException,
-                                   javax.servlet.ServletException {
-        System.out.println("Subscription");
-    }
-
-    public List<PermalinkProjectAction.Permalink> getPermalinks() {
-
-        List<PermalinkProjectAction.Permalink> lst = new ArrayList<PermalinkProjectAction.Permalink>();
-
-        PermalinkProjectAction.Permalink pl;
-        pl.
-
-
-        return lst;
-    }
- 
-    @Override
-    public String getUrlName() {
-        System.out.println("Url name");
-        return "test";
-    }
-
-    @Override
-    public String getDisplayName() {
-        System.out.println("Display name");
-        return "Test!!";
-    }
-
-    @Override
-    public String getIconFileName() {
-        System.out.println("Icon");
-        return "";
-    }
-
-
-}
-*/
